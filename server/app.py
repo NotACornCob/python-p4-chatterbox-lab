e7849f2fb60d3f1581ddffed5453d20dd8c57b8c
Alex <alex.kleinman@gmail.com>
2024-07-17 16:46:13 -0400

    partial completion

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, g
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -14,13 +14,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        response = make_response(
+            messages,
+            200,
+            {"Content-Type": "application/json"}
+        )
+        return response
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        body = data.get("body")
+        username = data.get("username")
+        created_at = data.get("created_at")
+        updated_at = data.get("updated_at")
+        new_message = Message(body=body, username=username, created_at=created_at, updated_at=updated_at)
+        db.session.add(new_message)
+        db.session.commit()
+        return new_message.to_dict(), 201
+
+@app.route('/messages/<int:id>, methods=["PATCH"], endpoint="message')
 def messages_by_id(id):
-    return ''
+    for attr in request.form:
+        setattr(message, attr, message.get(attr))
+    
+    db.session.add(message)
+    db.session.commit()
+
+    message_dict = message.to_dict()
+
+    response = make_response(
+        message_dict,
+        200
+    )
+
+    return response
+    
+    data = request.get_json
+    body = data.get("body")
+    message = Message.query.get(id)
+    return make_response(g.message.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/eb3de4a6b066_init_database.py
@@ -0,0 +1,24 @@
+"""init database
+
+Revision ID: eb3de4a6b066
+Revises: 
+Create Date: 2024-07-17 16:00:35.431345
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb3de4a6b066'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
